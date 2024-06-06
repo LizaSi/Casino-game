018c5d68b5ada63f32f3bdeb86c84d01afb4ec54
LizaSi <93578063+LizaSi@users.noreply.github.com>
2024-06-06 19:30:14 +0300

    Add files via upload

---
+++ BlackJack_new/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Card
+{
+    public enum Suit { Hearts, Diamonds, Clubs, Spades }
+    public enum Rank { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
+
+    public Suit CardSuit { get; private set; }
+    public Rank CardRank { get; private set; }
+
+    public Card(Suit suit, Rank rank)
+    {
+        CardSuit = suit;
+        CardRank = rank;
+    }
+
+    public int GetValue()
+    {
+        if (CardRank <= Rank.Ten)
+        {
+            return (int)CardRank;
+        }
+        else if (CardRank <= Rank.King)
+        {
+            return 10;
+        }
+        else // Ace
+        {
+            return 11;
+        }
+    }
+}
